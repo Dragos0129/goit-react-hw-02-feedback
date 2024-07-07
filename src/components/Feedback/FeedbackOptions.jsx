c059a663b83de95cc12af28d2a5808223c4806f3
Dragos0129 <dragosbaba@gmail.com>
2024-07-07 17:31:17 +0300

    commit 1

---
+++ src/components/Feedback/FeedbackOptions.jsx
@@ -0,0 +1,34 @@
+import style from './FeedbackOptions.module.css';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class FeedbackOptions extends Component {
+  static defaultProps = {
+    options: [],
+  };
+  static propTypes = {
+    options: PropTypes.array.isRequired,
+    onLeaveFeedBack: PropTypes.func.isRequired,
+  };
+
+  render() {
+    return (
+      <div className={style.feedback}>
+        <h1 className={style.feedback_title}>Please leave feedback</h1>
+        <div className={style.feedback_buttons}>
+          {this.props.options.map(btn => (
+            <button
+              className={style.feedback_buttons}
+              value={btn}
+              key={btn}
+              type="button"
+              onClick={e => this.props.onLeaveFeedBack(e)}
+            >
+              {btn}
+            </button>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
